5acf636be7289442ed00897fca5962a74c58692b
LetWillBe <balksamuri@gmail.com>
2021-05-30 11:17:24 +0300

    lesson 10

---
+++ labs/2/js/script2-10.js
@@ -0,0 +1,39 @@
+//УСЛОВИЯ
+
+'use strict';
+//----------------
+let a = '4';
+if (a === 4) {
+  console.log('OK!');
+} else {
+  console.log('ERROR');
+}
+//----------------
+const num = 50;
+if (num < 49) {
+  console.log('< 49');
+} else if (num > 100) {
+  console.log ('Больше 100');
+} else {
+  console.log ('50-99');
+}
+//Тернарный оператор - в его работе учавствует три аргумента
+(num === 50) ? console.log('50') : console.log('Error');
+
+//-----------------------
+//switch выполняет только строгое сравнение (===)
+const num1 = '50';
+switch (num1) {
+  case 49:/* Строгое сравнение num1 === 49 */
+    console.log('49');
+    break;/*если услови выполнилось - не переходить к проверке следующщих кейсов*/
+  case 100:
+    console.log('100');
+    break;
+  case 50:
+    console.log('50');
+    break;
+  default:/*Если не выполнилось ни одно условие */
+    console.log ('Не получилось');
+    break;
+}
