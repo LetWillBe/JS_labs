e8ec0319a494fa73e039d64f2aa08c07c9753564
LetWillBe <balksamuri@gmail.com>
2022-01-26 23:54:57 +0200

    повторил уроки 16-20

--- 2/js/16-function.js
+++ 2/js/16-function.js
@@ -4,16 +4,17 @@
 // function имяФункции(аргументы функции){код выполняемый функцией}
 // вызвать функцию можно до её появления в коде
 
-let num0 = 2; //глобальная перменная, её можно использовать внутри функции
-showFirstMessage("Hello, Max!");
+let num0 = 123; //глобальная перменная, её можно использовать внутри функции
 
 function showFirstMessage(text) {
     console.log(text);
-    let num = 20; //локальная перменна, видна и существует только внутри функции
+    let num = 789; //локальная перменна, видна и существует только внутри функции
     // num0 = 200;//меняем значение глобальной перменной
-    let num0 = 222; //значение глобальной перменной не изенится, num0 = 222только внутри функции
+    let num0 = 456; //значение глобальной перменной не изенится, num0 = 222только внутри функции
     console.log(num0); //результат - 222
 }
+showFirstMessage("Hello, Max!");
+
 console.log(num0); // результат - 2
 
 //замыкание – это функция + внешние переменные.
@@ -22,8 +23,9 @@ console.log(num0); // результат - 2
 let num1 = 2;
 
 function showFirstMessage(text) {
+    let num1 = 3;
     console.log(text);
-    console.log(num1); // 2
+    console.log(num1); // 3
 }
 showFirstMessage("Hello, Max!");
 console.log(num1); // 2
@@ -34,10 +36,10 @@ function calc(a, b) {
     return (a + b);
     console.log(a, b); //строка не выполнится никогда
 }
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(14, 43));
-//-----------------------
+console.log(calc(4, 3)); //7
+console.log(calc(5, 6)); //11
+console.log(calc(14, 43)); //57
+//Возвращение значения наружу
 function ret() {
     let num = 50;
     return num; //значение локальной переменной с помощью return передаётся из локальной области в глобальную
@@ -55,4 +57,6 @@ logger();
 //Стрелочные функции(добавлены в ES6)
 //не имеет контекста вызова
 // имяПеременной = (аргументы ф-ции) => {код};
-const calc1 = (a, b) => a + b;
+const calc1 = (a, b) => {
+    return a + b;
+};
--- 2/js/20-objects_for-in.js
+++ 2/js/20-objects_for-in.js
@@ -5,111 +5,122 @@
 //они могут так же быть вложены: объекты в объекты, массивы в объекты, функции в объекты
 
 //Создание объектов
-//синтаксис "конструктор объекта"
+//синтаксис "конструктор объекта"(устаревший)
 const obj = new Object();
 obj.name = `Max`;
 obj.age = `43`;
 obj.gender = `male`;
+console.log(obj);
 
 //актуальный способ создания объектов, синтаксис "литерал объекта"
 const options = {
-	name: `test`,
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: `black`,
-		background: `red`,
-	}
+    name: `test`,
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: `black`,
+        background: `red`,
+    }
 };
 
 //вызов свойств объекта
-console.log(options.name);//test
+console.log(options.name); //test
 
 //удаление свойств объекта
-delete options.name;
+//delete options.name;
 
-//Перебор свойств объекта for-in
+//for-in - перебор свойств объекта 
 //https://learn.javascript.ru/object-for-in
 for (let key in options) {
-	if (typeof(options[key]) === 'object') {//проверка если ключ объекта options сам явлется объектом
-		for (let i in options[key]) {
-			console.log(`свойство  ${i} имеет значение ${options[key][i]}`);
-			//перебор свойств объекта options[key] находящегося внутри объекта options
-		}
-	} else {
-		console.log(`свойство  ${key} имеет значение ${options[key]}`);
-	}
+    //перебираем свойства объекта options
+    console.log(`Свойство ${key} равно ${options[key]}`);
+    //проверка если ключ объекта options сам явлется объектом
+    if (typeof options[key] === 'object') {
+        //перебор свойств объекта options[key] находящегося внутри объекта options
+        for (let i in options[key]) {
+            console.log(`свойство  ${i} имеет значение ${options[key][i]}`);
+        }
+    } else {
+        //перебираем свойства объекта options
+        console.log(`свойство  ${key} имеет значение ${options[key]}`);
+    }
 }
 
 //функции и методы внутри объекта
 //https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Object#methods
+
+//подсчёт количества свойств объекта с помощью переменной counter
 const options1 = {
-	name: `test`,
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: `black`,
-		background: `red`,
-	}
+    name: `test`,
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: `black`,
+        background: `red`,
+    }
 };
 
 let counter = 0;
 
 for (let key in options1) {
-	if (typeof(options1[key]) === 'object') {//проверка если ключ объекта options сам явлется объектом
-		for (let i in options1[key]) {
-			console.log(`свойство  ${i} имеет значение ${options1[key][i]}`);
-			//перебор свойств объекта options[key] находящегося внутри объекта options
-			counter++;
-		}
-	} else {
-		console.log(`свойство  ${key} имеет значение ${options1[key]}`);
-		counter ++;
-	}
+    if (typeof(options1[key]) === 'object') { //проверка если ключ объекта options сам явлется объектом
+        for (let i in options1[key]) {
+            console.log(`свойство  ${i} имеет значение ${options1[key][i]}`);
+            //перебор свойств объекта options[key] находящегося внутри объекта options
+            counter++;
+        }
+    } else {
+        console.log(`свойство  ${key} имеет значение ${options1[key]}`);
+        counter++;
+    }
 }
 
-console.log(counter);//подсчёт количества свойств с помощью переменной counter
+console.log(counter); //количество свойств
 
 //подсчёт кол-ва свойств с поомщью Object.keys
 const options2 = {
-	name: `test`,
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: `black`,
-		background: `red`,
-	}
+    name: `test`,
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: `black`,
+        background: `red`,
+    }
 };
-console.log(Object.keys(options2));//получаем массив со всеми ключами
-console.log(Object.keys(options2).lenght);//получаем длину массива(кол-во ключей)
+console.log(Object.keys(options2)); //получаем массив со всеми ключами [ 'name', 'width', 'height', 'colors' ]
+console.log(Object.keys(options2).length); //получаем длину массива(кол-во ключей) - 4
 
 //Создание методов внутри объектов
+//Метод - это действие которое может совершать объект
 const options3 = {
-	name: `test`,
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: `black`,
-		background: `red`,
-	},
-	makeTest: function() {
-		console.log('Test');
-	}//созданный метод
+    name: `test`,
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: `black`,
+        background: `red`,
+    },
+    makeTest: function() {
+        console.log('Test');
+    } //созданный метод
 };
 
 options3.makeTest();
 
-//Деструктуризация объектов
-//переменные выносятся за объект в вид отдельных структур
+//Деструктуризация объектов (ES6)
+//переменные выносятся за объект в виде отдельных структур
 const options4 = {
-	name: `test`,
-	width: 1024,
-	height: 1024,
-	colors: {
-		border: `black`,
-		background: `red`,
-	}
+    name: `test`,
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: `black`,
+        background: `red`,
+    }
 };
-
-const {border, background} = options4.colors;
-console.log(border);//black
+//помещаем внутрь {} то что хотим вынести
+const {
+    border,
+    background
+} = options4.colors;
+console.log(border); //black
