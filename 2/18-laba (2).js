d6d7e0866fb0aaa2df56dd055cf64bbdc3e0c48a
LetWillBe <balksamuri@gmail.com>
2022-02-11 23:43:59 +0200

    Lesson18

--- 2/18-laba (2).js
+++ 2/18-laba (2).js
@@ -15,7 +15,17 @@ P.S. Функции вызывать не обязательно*/
 
 // Код возьмите из предыдущего домашнего задания
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -24,25 +34,33 @@ const personalMovieDB = {
     privat: false
 };
 
-if (numberOfFilms < 10) {
-    alert(`Просмотрено довольно мало фильмов`);
-} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
-    alert(`Вы классический зритель`);
-} else if (numberOfFilms >= 30) {
-    alert(`Вы киноман`);
-} else {
-    alert(`Что-то пошло не так!`);
-}
-
-for (let i = 1; i <= 3; ++i) {
-    let answer1 = prompt(`Один из просмотренных фильмов (${i} из 3)`, ''),
-        answer2 = prompt('На сколько оцените его?', '');
-    if (answer1 == null || answer2 == null || answer1 == '' || answer2 == '' || answer1.length > 50 || answer2.length > 50) {
-        --i;
-        alert(`Неверное значение или длина строки больше 50 символов`);
+function detectPersonalLevel() {
+    if (numberOfFilms < 10) {
+        alert(`Просмотрено довольно мало фильмов`);
+    } else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+        alert(`Вы классический зритель`);
+    } else if (numberOfFilms >= 30) {
+        alert(`Вы киноман`);
     } else {
-        personalMovieDB.movies[answer1] = answer2;
+        alert(`Что-то пошло не так!`);
     }
 }
+
+detectPersonalLevel();
+
+function rememberMyFilms() {
+    for (let i = 1; i <= 3; ++i) {
+        let answer1 = prompt(`Один из просмотренных фильмов (${i} из 3)`, ''),
+            answer2 = prompt('На сколько оцените его?', '');
+        if (answer1 == null || answer2 == null || answer1 == '' || answer2 == '' || answer1.length > 50 || answer2.length > 50) {
+            --i;
+            alert(`Неверное значение или длина строки больше 50 символов`);
+        } else {
+            personalMovieDB.movies[answer1] = answer2;
+        }
+    }
+}
+
+rememberMyFilms();
 
 console.log(personalMovieDB);
