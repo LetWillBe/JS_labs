df80ba1879ab25d80ca8e55d589a477986368b17
LetWillBe <balksamuri@gmail.com>
2022-02-14 21:11:14 +0200

    Lesson28_repeat

--- 2/28 Получение элементов со страницы/js/script.js
+++ 2/28 Получение элементов со страницы/js/script.js
@@ -2,28 +2,28 @@
 
 const box = document.getElementById("box");
 
-console.log(box);//<div class="box" id="box">
+//console.log(box); //*<div class="box" id="box">
 
 const btns = document.getElementsByTagName('button');
 
-console.log(btns);//HTMLCollection [ <button>, <button>, <button>, <button>, <button> ] - получили псевдомассив с кнопками
+//console.log(btns); //*HTMLCollection [ <button>, <button>, <button>, <button>, <button> ] - получили псевдомассив с кнопками
 
-let btns1 = document.getElementsByTagName("button")[1];//так мы получаем непосредественно кусок HTMLкода с кнопокй и можем задвайть ей свойства и атрибуты
+let btns1 = document.getElementsByTagName("button")[1]; //*так мы получаем непосредественно кусок HTMLкода с кнопкой и можем задвать ей свойства и атрибуты
 
-console.log(btns1);//получили вторую кнопку <button>2</button>
+//console.log(btns1); //*получили вторую кнопку <button>2</button>
 
-console.log(btns[1]);//так же получили <button>2</button>
+//console.log(btns[1]); //*так же получили <button>2</button>
 
 const circles = document.getElementsByClassName('circle');
 
-console.log(circles);//HTMLCollection [ <div.circle>, <div.circle>, <div.circle> ]
+//console.log(circles); //*HTMLCollection [ <div.circle>, <div.circle>, <div.circle> ]
 
-const hearts = document.querySelectorAll(".heart");//получение элементов по CSS селектору
+const hearts = document.querySelectorAll(".heart"); //*получение элементов по CSS селектору
 
-console.log(hearts);//NodeList [ <div.heart>, <div.heart>, <div.heart> ]
+//console.log(hearts); //*NodeList [ <div.heart>, <div.heart>, <div.heart> ]
 
 hearts.forEach(item => {
-	console.log(item);
+    //console.log(item);
 });
 /*
 <div class="heart">
@@ -31,5 +31,5 @@ hearts.forEach(item => {
 <div class="heart">
 */
 
-const oneHeart = document.querySelector('.heart');//получает первый элемент с сответствующим классом
-console.log(oneHeart);//<div class="heart">
+const oneHeart = document.querySelector('.heart'); //*получает первый элемент с сответствующим классом
+//console.log(oneHeart); //*<div class="heart">
--- 2/29 Действия с элементами на странице/js/script.js
+++ 2/29 Действия с элементами на странице/js/script.js
@@ -1,96 +1,96 @@
 'use strict';
 
 const box = document.getElementById("box"),
-	btns = document.getElementsByTagName('button'),
-	circles = document.getElementsByClassName('circle'),
-	hearts = document.querySelectorAll('.heart'),
-	oneHeart = document.querySelector('.heart');
+    btns = document.getElementsByTagName('button'),
+    circles = document.getElementsByClassName('circle'),
+    hearts = document.querySelectorAll('.heart'),
+    oneHeart = document.querySelector('.heart');
 
-/*<div class="box" id="box" style="width: 500px; background-color: blue;"></div> */
-box.style.backgroundColor = 'blue';
-box.style.width = '500px';
+//*<div class="box" id="box" style="width: 500px; background-color: blue;"></div> 
+//box.style.backgroundColor = 'blue';
+//box.style.width = '500px';
 
-/* <button style="border-radius: 100%;">2</button> */
+//* <button style="border-radius: 100%;">2</button> 
 btns[1].style.borderRadius = '100%';
 
-/* <div class="circle" style="background-color: red;"></div>*/
-circles[0].style.backgroundColor = 'red';
+//* <div class="circle" style="background-color: red;"></div>
+//circles[0].style.backgroundColor = 'red';
 
-/* <div class="box" id="box" style="width: 250px; background-color: green;"></div> */
-box.style.cssText = 'background-color: green; width: 250px';
+//* <div class="box" id="box" style="width: 250px; background-color: green;"></div> 
+//box.style.cssText = 'background-color: green; width: 250px';
 
-/*Сделать у всех средец золотой фон*/
-/* <div class="heart" style="background-color: gold;"></div> */
+//!Сделать у всех средец золотой фон
+//* <div class="heart" style="background-color: gold;"></div> 
 for (let i = 0; i < hearts.length; i++) {
-	hearts[i].style.backgroundColor = "gold";
+    //hearts[i].style.backgroundColor = "gold";
 };
 
-/*Второй способ поменять фон у всех сердец*/
-/* <div class="heart" style="background-color: blue;"></div> */
+//!Второй способ поменять фон у всех сердец
+//* <div class="heart" style="background-color: blue;"></div>
 hearts.forEach(item => {
-	item.style.backgroundColor = 'blue';
-	});
+    //item.style.backgroundColor = 'blue';
+});
 
-/* Создать <div></div> */
-const div = document.createElement('div');
+//* Создать <div></div> 
+//const div = document.createElement('div');
 
-/* Вставка текста*/
-const text = document.createTextNode('Вставил текст');
+//* Вставка текста
+//const text = document.createTextNode('Вставил текст');
 
-/* Добавить к div класс black*/
-div.classList.add('black');
+//* Добавить к div класс black
+//div.classList.add('black');
 
-/* Добавить div в конец body */
-document.body.append(div);//современный способ
-document.body.appendChild(div);//старый способ
+//* Добавить div в конец body 
+//document.body.append(div); //современный способ
+//document.body.appendChild(div); //старый способ
 
-/* Добавить div в конец элемента с классом .wrapper */
-document.querySelector('.wrapper').append(div);
+//* Добавить div в конец элемента с классом .wrapper 
+//document.querySelector('.wrapper').append(div);
 
-/*второй способ*/
-let wrapper = document.querySelector('.wrapper');
-wrapper.append(div);
+//*второй способ
+//let wrapper = document.querySelector('.wrapper');
+//wrapper.append(div);
 
-/*Вставить в начале wrapper элемент div*/
+//*Вставить в начале wrapper элемент div
 // wrapper.prepend(div);
 
-/*перед hearts[0] вставить div*/
+//*перед hearts[0] вставить div
 // hearts[0].before(div);
 
-//после hearts[0] вставить div
+//*после hearts[0] вставить div
 // hearts[0].after(div);
 
-/*до того как появились prepend, before/after */
-/*div вставляется в начало псевдомассива hearts, т.е. перед hearts[0]*/
+//! до того как появились prepend, before/after 
+//*div вставляется в начало псевдомассива hearts, т.е. перед hearts[0]
 // wrapper.insertBefore(div, hearts[0]);
 
-/*удалить circles[0]*/
+//*удалить circles[0]
 // circles[0].remove();
 // wrapper.removeChild(hearts[1]);//старый метод
 
 
-/*заменить один элемент другим*/
+//*заменить один элемент другим
 /*вместо hearts[0] поставить circles[0]*/
 // hearts[0].replaceWith(circles[0]);
 // wrapper.replaceChild(circles[0], hearts[0]);//старый метод
 
-/* в блок div вставить текст */
-div.innerHTML = "<h1>Hello!</h1>";
+//* в блок div вставить текст 
+//div.innerHTML = "<h1>Hello!</h1>";
 
-/*воспримет не как заголовок, а как обычный текст*/
+//*воспримет не как заголовок, а как обычный текст
 // div.textContent = "<h1>Hello!</h1>";
 
-/* вставка HTML-элемента перед div*/
-div.insertAdjacentHTML('beforebegin', '<h2>Max</h2>');
+//* вставка HTML-элемента перед div
+//div.insertAdjacentHTML('beforebegin', '<h2>Max</h2>');
 
-/*помещает код в начало элеента div */
-div.insertAdjacentHTML('afterbegin', '<h3>Well done!</h3>');
+//*помещает код в начало элеента div 
+//div.insertAdjacentHTML('afterbegin', '<h3>Well done!</h3>');
 
-/*помещает код в конец элемента div */
-div.insertAdjacentHTML('beforeend', '<h4>ОК!</h4>');
+//*помещает код в конец элемента div 
+//div.insertAdjacentHTML('beforeend', '<h4>ОК!</h4>');
 
-/* вставка HTML-элемента после div*/
-div.insertAdjacentHTML('afterend', '<h5>The END!</h5>');
+//* вставка HTML-элемента после div
+//div.insertAdjacentHTML('afterend', '<h5>The END!</h5>');
 /*
 <h2>Max</h2>
 <div class="black">
@@ -100,11 +100,3 @@ div.insertAdjacentHTML('afterend', '<h5>The END!</h5>');
 </div>
 <h5>The END!</h5>
 */
-
-
-
-
-
-
-
-
