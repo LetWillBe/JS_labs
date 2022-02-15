dca536798ad5359f7b7bcbfd89a634f13a7bcc1c
LetWillBe <balksamuri@gmail.com>
2022-02-15 21:01:59 +0200

    Lesson30_start

--- 2/30 Практика. Задание на отработку действий со страницей/js/script0.js
+++ 2/30 Практика. Задание на отработку действий со страницей/js/script0.js
@@ -8,7 +8,7 @@
 Реализовать только при помощи JS
 
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
-Отсортировать их по алфавиту 
+Отсортировать их по алфавиту
 
 5) Добавить нумерацию выведенных фильмов */
 
@@ -24,3 +24,35 @@ const movieDB = {
     ]
 };
 
+//1
+const adv =document.querySelectorAll(".promo__adv img");
+adv.forEach( item => {
+	item.remove();
+});
+
+//2
+const genre = document.querySelector(".promo__genre");
+genre.textContent = "драма";
+
+//3
+const bg = document.querySelector(".promo__bg");
+bg.style.cssText="background-image: url(img/bg.jpg)";
+
+//4
+movieDB.movies.sort();
+
+//5
+const promo = document.querySelectorAll(".promo__interactive-item");
+promo.forEach((item, i) => {
+	item.textContent = `${i+1} ` + movieDB.movies[i];
+})
+
+// const movieList = document.querySelector(".promo__interactive-list");
+// movieList.innerHTML = "";
+// movieDB.movies.forEach((film, i) => {
+// 	movieList.innerHTML += `
+// 		<li class="promo__interactive-item">${i+1} ${film}
+// 			<div class="delete"></div>
+// 		</li>
+// 	`;
+// })
