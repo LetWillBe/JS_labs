b45b460905dec4008a626771e6fd753c1b3bbd24
LetWillBe <balksamuri@gmail.com>
2022-02-19 00:35:33 +0200

    Lesson33_rep

--- 2/33 Практика. Используем события на странице проекта/js/script.js
+++ 2/33 Практика. Используем события на странице проекта/js/script.js
@@ -8,7 +8,7 @@
 
 //2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-//3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
 //4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
 //"Добавляем любимый фильм"
@@ -28,59 +28,69 @@ const movieDB = {
     ]
 };
 
-let promoRemove = document.querySelectorAll(".promo__adv *");
-promoRemove.forEach((item) => {
-    item.remove();
-});
-
-let changeGenre = document.querySelector(".promo__genre");
-changeGenre.textContent = "драма";
-
-let changeBackground = document.querySelector(".promo__bg");
-changeBackground.style.backgroundImage = 'url("img/bg.jpg")';
-
+let ads = document.querySelectorAll(".promo__adv *");
+let genre = document.querySelector(".promo__genre");
+let background = document.querySelector(".promo__bg");
 let filmsList = document.querySelector(".promo__interactive-list");
-let movieListUpdate = () => {
-    movieDB.movies.sort();
-    filmsList.innerHTML = "";
-    movieDB.movies.forEach((item, index) => {
-        filmsList.insertAdjacentHTML('beforeend', `
+let form = document.querySelector("form.add");
+let input = form.querySelector(".adding__input");
+let btn = form.querySelector("button");
+let checkbox = form.querySelector("[type='checkbox']");
+
+let removeAds = (block) => {
+    block.forEach((item) => {
+        item.remove();
+    });
+};
+
+let changeGenre = (elem) => {
+    elem.textContent = "драма";
+};
+
+let changeBg = (bg) => {
+    bg.style.backgroundImage = 'url("img/bg.jpg")';
+};
+
+let redrawFilmsList = (moviesArr, list) => {
+    moviesArr.sort();
+    list.innerHTML = "";
+    moviesArr.forEach((item, index) => {
+        list.insertAdjacentHTML('beforeend', `
 	<li class="promo__interactive-item">${index + 1}. ${item}<div class="delete"></div></li>
 	`);
     });
-    //? При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-    let garbage = document.querySelectorAll(".delete");
-    garbage.forEach((btn, index) => {
-        btn.addEventListener("click", () => {
-            btn.parentElement.remove();
-            movieDB.movies.splice(index, 1);
-            movieListUpdate();
+    let delFilm = filmsList.querySelectorAll(".delete");
+    delFilm.forEach((item, index) => {
+        item.addEventListener('click', () => {
+            item.parentElement.remove();
+            moviesArr.splice(index, 1);
+            redrawFilmsList(moviesArr, list);
         });
     });
 };
 
-
-movieListUpdate();
-
-//? при клике на кнопку добавляем введённое значение в movieDB.movies
-let btn = document.querySelector(".add button"); //это кнопка Подтвердить
-let input = document.querySelector(".adding__input"); //поле ввода
-btn.addEventListener('click', (event) => {
-    event.preventDefault(); //? отменил перезагрузку страницы при клике на кнопку
-    if (input.value) {
-        if (input.value.length > 21) {
-            input.value = input.value.slice(0, 21) + "...";
+let addFilms = (filmsArr, newFilm) => {
+    btn.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (newFilm.value) {
+            if (newFilm.value.length > 21) {
+                newFilm.value = `${newFilm.value.slice(0,21)}...`;
+            }
+            filmsArr.push(newFilm.value.toUpperCase());
+            redrawFilmsList(filmsArr, filmsList);
+            newFilm.value = "";
+            if (checkbox.checked) {
+                console.log("Добавляем любимый фильм");
+            }
         }
-        //movieDB.movies[movieDB.movies.length] = input.value.toUpperCase();
-        movieDB.movies.push(input.value.toUpperCase()); //? потому что список весь заглавными буквами
-        input.value = ""; //? очистка поля ввода
-        movieDB.movies.sort(); //? сортируем массив заново
-        //? перестраиваем список
-        movieListUpdate();
-        //? Чекбокс "сделать фильм любимым"
-        let checkBox = document.querySelector('input[type=checkbox]');
-        if (checkBox.checked) {
-            console.log(`Добавляем любимый фильм`);
-        }
-    }
-});
+    });
+};
+
+removeAds(ads);
+changeGenre(genre);
+changeBg(background);
+redrawFilmsList(movieDB.movies, filmsList);
+addFilms(movieDB.movies, input);
+
+//! Сделал всё сам, кроме 3го задания
+//! забыл про parentElement.remove() и синтаксис splice(index,1)
---
+++ 2/33 Практика. Используем события на странице проекта/js/script0.js
@@ -0,0 +1,89 @@
+/* Задания на урок:
+
+//1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+//новый фильм добавляется в список. Страница не должна перезагружаться.
+//Новый фильм должен добавляться в movieDB.movies.
+//Для получения доступа к значению input - обращаемся к нему как input.value;
+//P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+//2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+//3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+//4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+//"Добавляем любимый фильм"
+
+//5) Фильмы должны быть отсортированы по алфавиту */
+
+'use strict';
+
+// Возьмите свой код из предыдущей практики
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+let promoRemove = document.querySelectorAll(".promo__adv *");
+promoRemove.forEach((item) => {
+    item.remove();
+});
+
+let changeGenre = document.querySelector(".promo__genre");
+changeGenre.textContent = "драма";
+
+let changeBackground = document.querySelector(".promo__bg");
+changeBackground.style.backgroundImage = 'url("img/bg.jpg")';
+
+let filmsList = document.querySelector(".promo__interactive-list");
+let movieListUpdate = () => {
+    movieDB.movies.sort();
+    filmsList.innerHTML = "";
+    movieDB.movies.forEach((item, index) => {
+        filmsList.insertAdjacentHTML('beforeend', `
+	<li class="promo__interactive-item">${index + 1}. ${item}<div class="delete"></div></li>
+	`);
+    });
+    //? При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+    let garbage = document.querySelectorAll(".delete");
+    garbage.forEach((btn, index) => {
+        btn.addEventListener("click", () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(index, 1);
+            movieListUpdate();
+        });
+    });
+};
+
+
+movieListUpdate();
+
+//? при клике на кнопку добавляем введённое значение в movieDB.movies
+let btn = document.querySelector(".add button"); //это кнопка Подтвердить
+let input = document.querySelector(".adding__input"); //поле ввода
+btn.addEventListener('click', (event) => {
+    event.preventDefault(); //? отменил перезагрузку страницы при клике на кнопку
+    if (input.value) {
+        if (input.value.length > 21) {
+            input.value = input.value.slice(0, 21) + "...";
+        }
+        //movieDB.movies[movieDB.movies.length] = input.value.toUpperCase();
+        movieDB.movies.push(input.value.toUpperCase()); //? потому что список весь заглавными буквами
+        input.value = ""; //? очистка поля ввода
+        movieDB.movies.sort(); //? сортируем массив заново
+        //? перестраиваем список
+        movieListUpdate();
+        //? Чекбокс "сделать фильм любимым"
+        let checkBox = document.querySelector('input[type=checkbox]');
+        if (checkBox.checked) {
+            console.log(`Добавляем любимый фильм`);
+        }
+    }
+});
+
+//Первый вариант
+//Сделал сам с небольшимии подсказками
